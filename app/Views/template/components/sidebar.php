2bd3726890bfe759776f0f80aae9808fa9479cd1
Annisa Arsya <annisa.alwidiaarsyanti2034@students.unila.ac.id>
2022-10-25 12:59:19 +0700

    fix crlf

--- app/Views/template/components/sidebar.php
+++ app/Views/template/components/sidebar.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
@@ -61,60 +60,4 @@
     <div class="text-center d-none d-md-inline">
         <button class="rounded-circle border-0" id="sidebarToggle"></button>
     </div>
-=======
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-    <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <img src="\assets\washing-machine.png" width="35px" alt="brand">
-        </div>
-        <div class="sidebar-brand-text mx-3">E-Laundry</div>
-    </a>
-    <!-- Divider -->
-    <hr class="sidebar-divider my-0">
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
-        <a class="nav-link" href="/home">
-        <i class="fas fa-fw fa-tachometer-alt"></i>
-        <span>Dashboard</span></a>
-    </li>
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-    <!-- Nav Item - Tables -->
-    <li class="nav-item">
-        <a class="nav-link" href="/transaksi">
-        <i class="fas fa-fw fa-shopping-cart"></i>
-        <span>Transaksi</span></a>
-    </li>
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-    <!-- Nav Item - Tables -->
-    <li class="nav-item">
-        <a class="nav-link" href="/layanan">
-        <i class="fas fa-fw fa-table"></i>
-        <span>Jenis Layanan</span></a>
-    </li>
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-    <!-- Nav Item - Tables -->
-    <li class="nav-item">
-        <a class="nav-link" href="#">
-        <i class="fas fa-fw fa-book"></i>
-        <span>Laporan Pendapatan</span></a>
-    </li>
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-    <!-- Nav Item - Tables -->
-    <li class="nav-item">
-        <a class="nav-link" href="#">
-        <i class="fas fa-fw fa-cog"></i>
-        <span>About</span></a>
-    </li>
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
->>>>>>> 40c6e1335f67742607b6d7709a60627b45eabd4b
 </ul>
