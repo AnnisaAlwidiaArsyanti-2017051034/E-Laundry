6887f4f1722ebcb97f80684576bab4417fce0382
irma1012 <83545590+irma1012@users.noreply.github.com>
2022-10-25 10:45:29 +0700

    25/10

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -1,26 +1,30 @@
 <?= $this->extend('template/template'); ?>
 <?= $this->section('content'); ?>
-<table>
-    <tr>
-        <td>Annisa Alwidia Arsyanti</td>
-        <td>2017051034</td>
-        <td>A</td>        
-    </tr>            
-    <tr>
-        <td>Kania Ratu Syifa Az Zahra</td>
-        <td>2057051014</td>
-        <td>B</td>
-    </tr>
-    <tr>
-        <td>Irma Azizah</td>
-        <td>2017051038</td>
-        <td>B</td>
-    </tr>
-    <tr>
-        <td>Silvia Rukmana</td>
-        <td>2017051047</td>
-        <td>A</td>
-    </tr>                
-</table>
+<div class="jumbotron jumbotron-fluid bg-info">
+    <div class="container text-white text-center">
+        <table>
+            <tr>
+                <th>Annisa Alwidia Arsyanti</th>
+                <th>2017051034</th>
+                <th>A</th>
+            </tr>
+            <tr>
+                <th>Kania Ratu Syifa Az Zahra</th>
+                <th>2057051014</th>
+                <th>B</th>
+            </tr>
+            <tr>
+                <th>Irma Azizah</th>
+                <th>2017051038</th>
+                <th>B</th>
+            </tr>
+            <tr>
+                <th>Silvia Rukmana</th>
+                <th>2017051047</th>
+                <th>A</th>
+            </tr>
+        </table>
+    </div>
+</div>
 
 <?= $this->endSection(); ?>
--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -1,4 +1,72 @@
-<?= $this->extend('template/template'); ?>
+<!-- Content Row --><?= $this->extend('template/template'); ?>
 <?= $this->section('content'); ?>
-<h2>Kelompok E-Laundry</h2>
-<?= $this->endSection(); ?>
+<div class="container-fluid">
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+    </div>
+    <!-- Content Row -->
+    <div class="row">
+        <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box">
+                <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">CPU Traffic</span>
+                    <span class="info-box-number">
+                        10
+                        <small>%</small>
+                    </span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box mb-3">
+                <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Likes</span>
+                    <span class="info-box-number">41,410</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+
+        <!-- fix for small devices only -->
+        <div class="clearfix hidden-md-up"></div>
+
+        <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box mb-3">
+                <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Sales</span>
+                    <span class="info-box-number">760</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+        <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box mb-3">
+                <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">New Members</span>
+                    <span class="info-box-number">2,000</span>
+                </div>
+                <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+        </div>
+        <!-- /.col -->
+    </div>
+
+</div>
+<?= $this->endSection() ?>
