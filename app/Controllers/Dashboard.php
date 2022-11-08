dcde767efaf532302ef626306c57d344c27249e9
Annisa Arsya <annisa.alwidiaarsyanti2034@students.unila.ac.id>
2022-11-08 15:01:55 +0700

    fix 8/11

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 namespace App\Controllers;
@@ -26,32 +25,3 @@ class Dashboard extends BaseController
     //--------------------------------------------------------------------
 
 }
-=======
-<?php
-
-namespace App\Controllers;
-
-use App\Models\User;
-
-class Dashboard extends BaseController
-{
-    public function __construct()
-    {
-        $this->userModel = new User();
-    }
-
-    public function index()
-    {
-        $data = [
-            'title' => 'Dashboard',
-            'user_logged_in' => $this->userModel->find($this->session->get('id')),
-            'session' => \Config\Services::session()
-        ];
-
-        return view('dashboard/home', $data);
-    }
-
-    //--------------------------------------------------------------------
-
-}
->>>>>>> 5dcbec202b6a21da831f460619e6ad883d9563cd
